c60f44976d3d1f36f2d6e429673a5e801b710b89
Natehhggh <nate_blau@yahoo.com>
2016-10-20 17:39:53 -0700

    Gets Random Numbers Correctly and a random correct answer

--- MathTestor/MathTestor.cpp
+++ MathTestor/MathTestor.cpp
@@ -6,6 +6,8 @@
 
 using namespace std;
 
+
+//Menu Functions
 void drawMenu(int);
 void optionsMenu();
 void additionMenu(int, char[]);
@@ -13,17 +15,24 @@ void subtractionMenu(int, char[]);
 void multiplicationMenu(int, char[]);
 void divisionMenu(int, char[]);
 
+//Functional Functions
 void addition(int);
 void subtraction(int);
 void multiplication(int);
 void division(int);
 
+//Framework Functions
+void displayAnswers(char, int);
+void displayEquation(char);
+char getCorrectAnswer();
+void getEquation(int, char);
 int getRandomNumber(int);
 char getChoice(char[], int);
 bool verifyChoice(char[], char, int);
 string getDifficulty(int);
 int changeDifficulty(int);
 
+//Enums
 enum Menus
 {
 	MainMenu,
@@ -42,18 +51,28 @@ enum Difficulty
 	Hard
 };
 
+//Constant
 const int MAINMENUCHOICES = 6;
 const int OPTIONSMENUCHOICES = 5;
 const int  ARITMATICMENUCHOICES= 3;
+const int NUMBEROFANSWERS = 5;
 
+//Global Array
 char mainMenuChoices[MAINMENUCHOICES] = { 'a', 'b', 'c', 'd', 'o', 'q'  };
 char optionsMenuChoices[OPTIONSMENUCHOICES] = { 'a', 's', 'm', 'd', 'r' };
 char arithmaticMenuChoices[ARITMATICMENUCHOICES] = { 'a', 'd', 'r' };
+char listOfAnswers[NUMBEROFANSWERS] = { 'a', 'b', 'c', 'd' , 'e'};
 
+//Difficulty Settings
 int additionDifficulty = Beginner;
 int subtractionDifficulty = Beginner;
 int multiplicationDifficulty = Beginner;
 int divisionDifficulty = Beginner;
+
+//Global Variables
+int x, y;
+string answer;
+char currentAnswer;
 
 int main(){
 	srand(time(NULL));
@@ -174,6 +193,7 @@ void drawMenu(int menu)
 		cout << arithmaticMenuChoices[0] << ": Addition" << endl;
 		cout << arithmaticMenuChoices[1] << ": Addition Difficulty : " << getDifficulty(additionDifficulty) << endl;
 		cout << arithmaticMenuChoices[2] << ": Return to Main Menu" << endl;
+		cout << "Enter your choice [ ";
 		for (int i = 0; i < ARITMATICMENUCHOICES; i++)
 		{
 			cout << arithmaticMenuChoices[i] << " ";
@@ -187,6 +207,7 @@ void drawMenu(int menu)
 		cout << arithmaticMenuChoices[0] << ": Subtraction" << endl;
 		cout << arithmaticMenuChoices[1] << ": Subtraction Difficulty : " << getDifficulty(subtractionDifficulty) << endl;
 		cout << arithmaticMenuChoices[2] << ": Return to Main Menu" << endl;
+		cout << "Enter your choice [ ";
 		for (int i = 0; i < ARITMATICMENUCHOICES; i++)
 		{
 			cout << arithmaticMenuChoices[i] << " ";
@@ -198,6 +219,7 @@ void drawMenu(int menu)
 		cout << arithmaticMenuChoices[0] << ": Multiplication" << endl;
 		cout << arithmaticMenuChoices[1] << ": Multiplication Difficulty : " << getDifficulty(multiplicationDifficulty) << endl;
 		cout << arithmaticMenuChoices[2] << ": Return to Main Menu" << endl;
+		cout << "Enter your choice [ ";
 		for (int i = 0; i < ARITMATICMENUCHOICES; i++)
 		{
 			cout << arithmaticMenuChoices[i] << " ";
@@ -209,6 +231,7 @@ void drawMenu(int menu)
 		cout << arithmaticMenuChoices[0] << ": Division" << endl;
 		cout << arithmaticMenuChoices[1] << ": Division Difficulty : " << getDifficulty(divisionDifficulty) << endl;
 		cout << arithmaticMenuChoices[2] << ": Return to Main Menu" << endl;
+		cout << "Enter your choice [ ";
 		for (int i = 0; i < ARITMATICMENUCHOICES; i++)
 		{
 			cout << arithmaticMenuChoices[i] << " ";
@@ -444,22 +467,120 @@ void divisionMenu(int, char choices[])
 	} while (running);
 }
 
+void getEquation(int difficulty, char type)
+{
+	x = getRandomNumber(difficulty);
+	y = getRandomNumber(difficulty);
+
+	switch (type)
+	{
+		//addition
+	case 'a':
+		answer =  to_string(x + y);
+		
+		
+		break;
+		//subtraction
+	case 'b':
+		answer = to_string(x - y);
+		
+		break;
+		//mutliplication
+	case 'c':
+		answer = to_string(x * y);
+		
+		break;
+		//division
+	case 'd':
+		if (y == 0)
+		{
+			answer = "undefined";
+		}
+		else
+		{
+			answer = to_string(x / y);
+		}
+		break;
+
+	default:
+		break;
+	}
+
+
+}
+
+void displayEquation(char type)
+{
+	cout << "    " << x << endl;
+	cout << "  " << type << " " << y << endl << endl;
+}
+
+void displayAnswers(char correct, int difficulty)
+{
+	currentAnswer = correct;
+	for (int i = 0; i < NUMBEROFANSWERS - 1; i++)
+	{
+		if (correct == listOfAnswers[i])
+		{
+			cout << listOfAnswers[i] << ": " << answer << endl;
+		}
+		else
+		{
+			cout << listOfAnswers[i] << ": " << getRandomNumber(difficulty) << endl;
+		}
+	}
+	cout << listOfAnswers[NUMBEROFANSWERS - 1] << ": None of the Above" << endl;
+
+}
+
+char getCorrectAnswer()
+{
+	return listOfAnswers[rand() % NUMBEROFANSWERS];
+}
+
 void addition(int difficulty)
 {
-	
+	getEquation(difficulty, 'a');
+
+	displayEquation('+');
+	displayAnswers(getCorrectAnswer(), difficulty);
+
+	cout << "Current Answer = " << currentAnswer << endl;
+	system("pause");
 }
 
 void subtraction(int difficulty)
 {
+	getEquation(difficulty, 'b');
 
+	displayEquation('-');
+	displayAnswers(getCorrectAnswer(), difficulty);
+
+	cout << "Current Answer = " << currentAnswer;
+
+	system("pause");
 }
 
 void multiplication(int difficulty)
 {
+	getEquation(difficulty, 'c');
 
+	displayEquation('*');
+	displayAnswers(getCorrectAnswer(), difficulty);
+
+	cout << "Current Answer = " << currentAnswer;
+
+	system("pause");
 }
 
 void division(int difficulty)
 {
+	getEquation(difficulty, 'd');
 
+	displayEquation('/');
+	displayAnswers(getCorrectAnswer(), difficulty);
+
+	cout << "Current Answer = " << currentAnswer;
+
+	system("pause");	
 }
